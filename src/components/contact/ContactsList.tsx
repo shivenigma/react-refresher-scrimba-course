6b05f260dc9dba7974474d87005ab1e854ebc8e0
Vignesh M <vicky@intermx.com>
2025-01-27 17:48:49 +0530

    made the data dynamic
    I know I should loop in the template, haven't come to that part in the course yet though.

--- src/components/contact/ContactsList.tsx
+++ src/components/contact/ContactsList.tsx
@@ -2,18 +2,25 @@ import {Contact} from './Contact';
 import cat from './assets/cat.png'
 
 export default function ContactsList() {
-    const data = {
+    const data = [
+        {
         name: 'Mr. Whiskerson',
         phone: '(212) 555-1234',
         email: 'wishkaz@gmail.com',
         img: cat
-    }
+    },
+        {
+        name: 'Mr. Doo',
+        phone: '(111) 555-1234',
+        email: 'DooCat@gmail.com',
+        img: cat
+    },
+
+    ]
     return (
         <div className="app-body-contacts">
-            <Contact data={data}></Contact>
-            <Contact data={data}></Contact>
-            <Contact data={data}></Contact>
-            <Contact data={data}></Contact>
+            <Contact data={data[0]}></Contact>
+            <Contact data={data[1]}></Contact>
         </div>
     )
 }
