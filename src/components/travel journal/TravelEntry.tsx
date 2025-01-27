e16a0eb02dad8d631cd95755d155563fb4a78075
Vignesh M <ms.vignesh31@gmail.com>
2025-01-27 19:35:30 +0530

    Feat: Finished the travel journal with actual data

--- src/components/travel journal/TravelEntry.tsx
+++ src/components/travel journal/TravelEntry.tsx
@@ -1,23 +1,23 @@
 import marker from './marker.png';
 
-export default function TravelEntry() {
+export default function TravelEntry({img, title, country, googleMapsLink, dates, text}) {
 return (
     <article className="travel-entry-item">
         <div className="travel-entry-image">
-            <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Japan"/>
+            <img src={img?.src} alt={img?.alt}/>
         </div>
         <div className="travel-entry-container">
             <div>
                 <div className="travel-entry-heading">
                     <img src={marker} alt="marker"/>
-                    <span>Japan</span>
-                    <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" target="_blank">View on Google Maps</a>
+                    <span>{country}</span>
+                    <a href={googleMapsLink} target="__blank">View on Google Maps</a>
                 </div>
-                <h1 className="travel-entry-destination">Mount Fuji</h1>
+                <h1 className="travel-entry-destination">{title}</h1>
             </div>
             <div className="travel-entry-content">
-                <p className="travel-entry-date">12 Jan, 2023 - 24 Jan, 2023</p>
-                <p className="travel-entry-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <p className="travel-entry-date">{dates}</p>
+                <p className="travel-entry-text">{text}</p>
             </div>
         </div>
     </article>
--- src/components/travel journal/TravelJournal.tsx
+++ src/components/travel journal/TravelJournal.tsx
@@ -1,15 +1,24 @@
 import './TravelJournal.css'
 import Header from './Header';
 import TravelEntry from './TravelEntry';
-
+import data from './travelData'
 export default function TravelJournal() {
     return (
         <div className="app-body-travel">
             <Header/>
             <div className="travel-container">
-                <TravelEntry/>
-                <TravelEntry/>
-                <TravelEntry/>
+                {data.map(item => {
+                    return <TravelEntry
+                        img={item.img}
+                        title={item.title}
+                        country={item.country}
+                        googleMapsLink={item.googleMapsLink}
+                        dates={item.dates}
+                        text={item.text}
+                        key={item.id}
+                    >
+                    </TravelEntry>
+                })}
             </div>
         </div>
     )
